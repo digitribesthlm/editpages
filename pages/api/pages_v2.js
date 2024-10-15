a4823d451d52532b326c888d2003b6726aefcf4b
digitribesthlm <patrik@makeablesthlm.se>
2024-10-15 18:08:03 +0200

    Change

--- pages/api/pages_v2.js
+++ pages/api/pages_v2.js
@@ -1,4 +1,5 @@
-import { connectToDatabase } from '../../lib/mongodb';
+// /pages/api/pages.js
+import clientPromise from '../../lib/mongodb';
 
 export default async function handler(req, res) {
   const { method, query: { accessId } } = req;
@@ -10,7 +11,8 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { db } = await connectToDatabase();
+    const client = await clientPromise;
+    const db = client.db("seotitledesc");
 
     const user = await db.collection("users").findOne({
       "accessTokens.token": accessId
@@ -24,7 +26,18 @@ export default async function handler(req, res) {
       const pages = await db.collection("pages").find({ companyId: user.companyId }).toArray();
       return res.status(200).json(pages);
     } else if (method === 'PUT') {
-      // ... (rest of your PUT logic)
+      const { pageId, ...updateData } = req.body;
+      if (!pageId) {
+        return res.status(400).json({ message: 'pageId is required for updates' });
+      }
+      const result = await db.collection("pages").updateOne(
+        { pageId: parseInt(pageId), companyId: user.companyId },
+        { $set: updateData }
+      );
+      if (result.matchedCount === 0) {
+        return res.status(404).json({ message: 'Page not found' });
+      }
+      return res.status(200).json({ message: 'Page updated successfully' });
     } else {
       res.setHeader('Allow', ['GET', 'PUT']);
       return res.status(405).json({ message: `Method ${method} Not Allowed` });
